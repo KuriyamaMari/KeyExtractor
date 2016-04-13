d7ec4301f91b5f057d12b5e45443ca11bc59d198
KuriyamaMari <KuriyamaMari@gmail.com>
2016-04-13 18:58:45 +0100

    First version

---
+++ KeyExtractor/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace KeyExtractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExtractKeys = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonExtractKeys
+            // 
+            this.buttonExtractKeys.Location = new System.Drawing.Point(101, 9);
+            this.buttonExtractKeys.Name = "buttonExtractKeys";
+            this.buttonExtractKeys.Size = new System.Drawing.Size(75, 23);
+            this.buttonExtractKeys.TabIndex = 1;
+            this.buttonExtractKeys.Text = "Extract Keys";
+            this.buttonExtractKeys.UseVisualStyleBackColor = true;
+            this.buttonExtractKeys.Click += new System.EventHandler(this.buttonExtractKeys_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 38);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(283, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 60);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonExtractKeys);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Key extractor";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonExtractKeys;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
+
---
+++ KeyExtractor/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace KeyExtractor
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            this.toolStripStatusLabel1.Text = "";
+            this.statusStrip1.Refresh();
+        }
+
+        /// <summary>
+        /// Extracts the keys from string.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <returns></returns>
+        private List<string> ExtractKeysFromString(string source)
+        {
+            MatchCollection m = Regex.Matches(source, "([0-9A-Z]{5})(?:\\-[0-9A-Z]{5}){2,3}",
+                  RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            List<string> result = new List<string>();
+            if (m.Count > 0)
+            {
+                foreach (Match v in m)
+                {
+                    result.Add(v.Value);
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Extracts the keys and copy to clipboard.
+        /// </summary>
+        private void ExtractKeysFromClipBoardAndCopyToClipboard()
+        {
+            string plainText;
+            plainText = Clipboard.GetText();
+
+            List<string> listStrKeys = ExtractKeysFromString(plainText);
+            if (listStrKeys.Count > 0)
+            {
+                string strKeys = string.Join(Environment.NewLine, listStrKeys.ToArray());
+                try
+                {
+                    Clipboard.SetText(strKeys);
+                    UpdateToolStripStatusLabel(string.Format("{0} keys have been copied to clipboard", listStrKeys.Count));
+                }
+                catch
+                {
+                    MessageBox.Show(strKeys, "Ctrl+C to copy");
+                }
+            }
+            else
+            {
+                UpdateToolStripStatusLabel(string.Format("No keys have been found."));
+            }
+        }
+
+        /// <summary>
+        /// Updates the tool strip status label.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        private void UpdateToolStripStatusLabel(string text)
+        {
+            this.toolStripStatusLabel1.Text = text;
+            this.statusStrip1.Refresh();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the buttonExtractKeys control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void buttonExtractKeys_Click(object sender, EventArgs e)
+        {
+            ExtractKeysFromClipBoardAndCopyToClipboard();
+        }
+    }
+}
