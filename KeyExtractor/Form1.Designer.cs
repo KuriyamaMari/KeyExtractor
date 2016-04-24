176d4b0be9e9bba15b2dc3fd8728c87e40c3c0e0
KuriyamaMari <KuriyamaMari>
2016-04-24 11:42:10 +0100

    Add support to latest ASF changes
    With the ASF latest update, a bug has been fixed that it's now forced to
    use comma instead of new lines when redeeming multiple keys.
    
    An option has been added to choose the mode between classic mode or ASF
    mode. The default option is ASF.

--- KeyExtractor/Form1.Designer.cs
+++ KeyExtractor/Form1.Designer.cs
@@ -28,54 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonExtractKeys = new System.Windows.Forms.Button();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.statusStrip1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // buttonExtractKeys
-            // 
-            this.buttonExtractKeys.Location = new System.Drawing.Point(101, 9);
-            this.buttonExtractKeys.Name = "buttonExtractKeys";
-            this.buttonExtractKeys.Size = new System.Drawing.Size(75, 23);
-            this.buttonExtractKeys.TabIndex = 1;
-            this.buttonExtractKeys.Text = "Extract Keys";
-            this.buttonExtractKeys.UseVisualStyleBackColor = true;
-            this.buttonExtractKeys.Click += new System.EventHandler(this.buttonExtractKeys_Click);
-            // 
-            // statusStrip1
-            // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+      this.buttonExtractKeys = new System.Windows.Forms.Button();
+      this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+      this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+      this.comboBoxMode = new System.Windows.Forms.ComboBox();
+      this.statusStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // buttonExtractKeys
+      // 
+      this.buttonExtractKeys.Location = new System.Drawing.Point(172, 7);
+      this.buttonExtractKeys.Name = "buttonExtractKeys";
+      this.buttonExtractKeys.Size = new System.Drawing.Size(75, 21);
+      this.buttonExtractKeys.TabIndex = 1;
+      this.buttonExtractKeys.Text = "Extract Keys";
+      this.buttonExtractKeys.UseVisualStyleBackColor = true;
+      this.buttonExtractKeys.Click += new System.EventHandler(this.buttonExtractKeys_Click);
+      // 
+      // statusStrip1
+      // 
+      this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 38);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(283, 22);
-            this.statusStrip1.TabIndex = 2;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // toolStripStatusLabel1
-            // 
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
-            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(283, 60);
-            this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.buttonExtractKeys);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "Form1";
-            this.Text = "Key extractor";
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.statusStrip1.Location = new System.Drawing.Point(0, 33);
+      this.statusStrip1.Name = "statusStrip1";
+      this.statusStrip1.Size = new System.Drawing.Size(283, 22);
+      this.statusStrip1.TabIndex = 2;
+      this.statusStrip1.Text = "statusStrip1";
+      // 
+      // toolStripStatusLabel1
+      // 
+      this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+      this.toolStripStatusLabel1.Size = new System.Drawing.Size(131, 17);
+      this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+      // 
+      // comboBoxMode
+      // 
+      this.comboBoxMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.comboBoxMode.FormattingEnabled = true;
+      this.comboBoxMode.Items.AddRange(new object[] {
+            "Classic",
+            "ASF"});
+      this.comboBoxMode.Location = new System.Drawing.Point(41, 7);
+      this.comboBoxMode.Name = "comboBoxMode";
+      this.comboBoxMode.Size = new System.Drawing.Size(85, 20);
+      this.comboBoxMode.TabIndex = 3;
+      this.comboBoxMode.SelectedIndexChanged += new System.EventHandler(this.comboBoxMode_SelectedIndexChanged);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(283, 55);
+      this.Controls.Add(this.comboBoxMode);
+      this.Controls.Add(this.statusStrip1);
+      this.Controls.Add(this.buttonExtractKeys);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "Form1";
+      this.Text = "Key extractor";
+      this.statusStrip1.ResumeLayout(false);
+      this.statusStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
@@ -83,6 +98,7 @@
         private System.Windows.Forms.Button buttonExtractKeys;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ComboBox comboBoxMode;
     }
 }
 
--- KeyExtractor/Form1.cs
+++ KeyExtractor/Form1.cs
@@ -5,84 +5,119 @@ using System.Windows.Forms;
 
 namespace KeyExtractor
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+    /// <summary>
+    /// This sets what present mode it uses.
+    /// 0 - classic mode. Keys will be split into new lines
+    /// 1 - ASF mode. Keys will be split using comma.
+    /// </summary>
+    private int m_PresentMode = 0;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Form1"/> class.
+    /// </summary>
+    public Form1()
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Form1"/> class.
-        /// </summary>
-        public Form1()
-        {
-            InitializeComponent();
-            this.toolStripStatusLabel1.Text = "";
-            this.statusStrip1.Refresh();
-        }
-
-        /// <summary>
-        /// Extracts the keys from string.
-        /// </summary>
-        /// <param name="source">The source.</param>
-        /// <returns></returns>
-        private List<string> ExtractKeysFromString(string source)
-        {
-            MatchCollection m = Regex.Matches(source, "([0-9A-Z]{5})(?:\\-[0-9A-Z]{5}){2,3}",
-                  RegexOptions.IgnoreCase | RegexOptions.Singleline);
-            List<string> result = new List<string>();
-            if (m.Count > 0)
-            {
-                foreach (Match v in m)
-                {
-                    result.Add(v.Value);
-                }
-            }
-            return result;
-        }
-
-        /// <summary>
-        /// Extracts the keys and copy to clipboard.
-        /// </summary>
-        private void ExtractKeysFromClipBoardAndCopyToClipboard()
-        {
-            string plainText;
-            plainText = Clipboard.GetText();
-
-            List<string> listStrKeys = ExtractKeysFromString(plainText);
-            if (listStrKeys.Count > 0)
-            {
-                string strKeys = string.Join(Environment.NewLine, listStrKeys.ToArray());
-                try
-                {
-                    Clipboard.SetText(strKeys);
-                    UpdateToolStripStatusLabel(string.Format("{0} keys have been copied to clipboard", listStrKeys.Count));
-                }
-                catch
-                {
-                    MessageBox.Show(strKeys, "Ctrl+C to copy");
-                }
-            }
-            else
-            {
-                UpdateToolStripStatusLabel(string.Format("No keys have been found."));
-            }
-        }
-
-        /// <summary>
-        /// Updates the tool strip status label.
-        /// </summary>
-        /// <param name="text">The text.</param>
-        private void UpdateToolStripStatusLabel(string text)
-        {
-            this.toolStripStatusLabel1.Text = text;
-            this.statusStrip1.Refresh();
-        }
-
-        /// <summary>
-        /// Handles the Click event of the buttonExtractKeys control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        private void buttonExtractKeys_Click(object sender, EventArgs e)
-        {
-            ExtractKeysFromClipBoardAndCopyToClipboard();
-        }
+      InitializeComponent();
+      
+      // Update status stip.
+      this.toolStripStatusLabel1.Text = "";
+      this.statusStrip1.Refresh();
+      
+      // Default mode is ASF.
+      this.comboBoxMode.SelectedIndex = 1;
     }
+
+    /// <summary>
+    /// Extracts the keys from string.
+    /// </summary>
+    /// <param name="source">The source.</param>
+    /// <returns></returns>
+    private List<string> ExtractKeysFromString(string source)
+    {
+      MatchCollection m = Regex.Matches(source, "([0-9A-Z]{5})(?:\\-[0-9A-Z]{5}){2,3}",
+            RegexOptions.IgnoreCase | RegexOptions.Singleline);
+      List<string> result = new List<string>();
+      if (m.Count > 0)
+      {
+        foreach (Match v in m)
+        {
+          result.Add(v.Value);
+        }
+      }
+      return result;
+    }
+
+    /// <summary>
+    /// Extracts the keys and copy to clipboard.
+    /// </summary>
+    private void ExtractKeysFromClipBoardAndCopyToClipboard()
+    {
+      string plainText;
+      plainText = Clipboard.GetText();
+
+      List<string> listStrKeys = ExtractKeysFromString(plainText);
+      if (listStrKeys.Count > 0)
+      {
+        string strKeys;
+
+        switch (m_PresentMode)
+        {
+          case 1:
+            strKeys = string.Join(",", listStrKeys.ToArray());
+            break;
+          case 0:
+          default:
+            strKeys = string.Join(Environment.NewLine, listStrKeys.ToArray());
+            break;
+        }
+
+        try
+        {
+          Clipboard.SetText(strKeys);
+          UpdateToolStripStatusLabel(string.Format("{0} keys have been copied to clipboard", listStrKeys.Count));
+        }
+        catch
+        {
+          MessageBox.Show(strKeys, "Ctrl+C to copy");
+        }
+      }
+      else
+      {
+        UpdateToolStripStatusLabel(string.Format("No keys have been found."));
+      }
+    }
+
+    /// <summary>
+    /// Updates the tool strip status label.
+    /// </summary>
+    /// <param name="text">The text.</param>
+    private void UpdateToolStripStatusLabel(string text)
+    {
+      this.toolStripStatusLabel1.Text = text;
+      this.statusStrip1.Refresh();
+    }
+
+    /// <summary>
+    /// Handles the Click event of the buttonExtractKeys control.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    private void buttonExtractKeys_Click(object sender, EventArgs e)
+    {
+      ExtractKeysFromClipBoardAndCopyToClipboard();
+    }
+
+    /// <summary>
+    /// Handles the SelectedIndexChanged event of the comboBoxMode control.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+    private void comboBoxMode_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      m_PresentMode = this.comboBoxMode.SelectedIndex;
+      UpdateToolStripStatusLabel(this.comboBoxMode.SelectedItem.ToString() + " mode selected.");
+    }
+  }
 }
